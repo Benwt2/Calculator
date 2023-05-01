1ab7b40388459ad8f2d5a2c218a91cd529f244f1
Benwt2 <125859176+Benwt2@users.noreply.github.com>
2023-05-01 14:07:44 +0800

    新增程式
    讓程式可以+-*/=

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        float firstNumber, secondNumber;
+        int operators = -1;
         public MainWindow()
         {
             InitializeComponent();
@@ -37,7 +39,7 @@ namespace Calculator
 
         private void btnTwo_Click(object sender, RoutedEventArgs e)
         {
-            if (txtNumber.Text == "0") // 如果你的判斷式簡單到只有一行程式，可以把 { } 大刮號省略掉
+            if (txtNumber.Text == "0")
                 txtNumber.Text = "";
             txtNumber.Text = txtNumber.Text + "2";
         }
@@ -96,6 +98,75 @@ namespace Calculator
             if (txtNumber.Text == "0")
                 txtNumber.Text = "";
             txtNumber.Text = txtNumber.Text + "0";
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            firstNumber = Convert.ToSingle(txtNumber.Text);
+            txtNumber.Text = "0";
+            operators = 0;
+        }
+
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            firstNumber = Convert.ToSingle(txtNumber.Text);
+            txtNumber.Text = "0";
+            operators = 1;
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            firstNumber = Convert.ToSingle(txtNumber.Text);
+            txtNumber.Text = "0";
+            operators = 2;
+        }
+
+        private void btnDivide_Click(object sender, RoutedEventArgs e)
+        {
+            firstNumber = Convert.ToSingle(txtNumber.Text);
+            txtNumber.Text = "0";
+            operators = 3;
+        }
+
+        private void btnDot_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtNumber.Text.IndexOf(".") == -1)
+                txtNumber.Text = txtNumber.Text + ".";
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtNumber.Text = "0";
+            firstNumber = 0f;
+            secondNumber = 0f;
+            operators = -1;
+        }
+
+        private void btnEqual_Click(object sender, RoutedEventArgs e)
+        {
+            float finalResults = 0f;
+            secondNumber = Convert.ToSingle(txtNumber.Text);
+
+            switch (operators)
+            {
+                case 0:
+                    finalResults = firstNumber + secondNumber;
+                    break;
+                case 1:
+                    finalResults = firstNumber - secondNumber;
+                    break;
+                case 2:
+                    finalResults = firstNumber * secondNumber;
+                    break;
+                case 3:
+                    finalResults = firstNumber / secondNumber;
+                    break;
+            }
+            txtNumber.Text = string.Format("{0:0.##########}", finalResults);
+
+            firstNumber = 0f;
+            secondNumber = 0f;
+            operators = -1;
         }
     }
 }
